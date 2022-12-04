4e882c548c616c12123211e981105bdde369d6ca
pauldreyer206 <paulroux.dreyer@gmail.com>
2022-12-04 20:36:36 +0200

    Day 4

--- main.c
+++ main.c
@@ -2,86 +2,149 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// day 4
 
-// day 3
-int find_common_char(char *arr1, char *arr2, int len) {
-  int i = 0, j = 0;
-  for (i = 0; i < len; i++) {
-    for (j = 0; j < len; j++) {
-      if (arr1[i] == arr2[j]) {
-        if (arr1[i] < 91) {
-          printf("Common char: %c Value: %d\n", arr1[i],
-                 (int)(arr1[i] - 38)); // use some ascii magic to get the value
-          return (int)(arr1[i] - 38);
-        } else {
-          printf("Common char: %c Value: %d\n", arr1[i], (int)(arr1[i] - 96));
-          return (int)(arr1[i] - 96);
-        }
-      }
-    }
-  }
-  return 1;
+int range_contains_other_range(int min, int max, int min2, int max2) { // part 1
+  return (min >= min2 && min <= max2) || (max >= min2 && max <= max2);
 }
 
-int find_common_char_3_arrays(char *arr1, char *arr2, char *arr3, int len1,
-                              int len2, int len3) {
-  int i = 0, j = 0, k = 0;
-  for (i = 0; i < len1; i++) {
-    for (j = 0; j < len2; j++) {
-      for (k = 0; k < len3; k++) {
-        if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
-          if (arr1[i] < 91) {
-            // printf("Common char: %c Value: %d\n", arr1[i], (int)(arr1[i] -
-            // 38));
-            return (int)(arr1[i] - 38); // use some ascii magic to get the value
-          } else {
-            // printf("Common char: %c Value: %d\n", arr1[i], (int)(arr1[i] -
-            // 96));
-            return (int)(arr1[i] - 96);
-          }
-        }
-      }
-    }
-  }
-  return 1;
+int range_overlaps_other_range(int min, int max, int min2, int max2) { // part 2
+  return (min >= min2 && min <= max2) || (max >= min2 && max <= max2) ||
+         (min2 >= min && min2 <= max) || (max2 >= min && max2 <= max);
 }
-// part 2
+
+struct range {
+  int min;
+  int max;
+};
+
+struct range ranges[1000][2];
+
+// part one
 int main() {
   FILE *fptr;
-  int answer = 0;
   char str[60];
-  char lines[300][100];
-  int line_lenghts[300];
   int i = 0;
   int j = 0;
+  int answer = 0;
+  char *token;
 
   if ((fptr = fopen("C:\\input.txt", "r")) == NULL) {
     printf("Error! opening file");
     return 0;
   }
-
   while (fgets(str, 60, fptr) != NULL) { // put data in array
-    while (str[j] != '\0') {
-      lines[i][j] = str[j];
-      j++;
-    }
-    if (str[j] == '\0') {
-      lines[i][j - 1] = '\0';
-      line_lenghts[i] = j - 1;
-    }
-    j = 0;
+    printf("%s", str);
+    token = strtok(str, ",-");
+    printf("%s\n", token);
+    ranges[i][0].min = atoi(token);
+    token = strtok(NULL, ",-");
+    printf("%s\n", token);
+    ranges[i][0].max = atoi(token);
+    token = strtok(NULL, ",-");
+    printf("%s\n", token);
+    ranges[i][1].min = atoi(token);
+    token = strtok(NULL, ",-");
+    printf("%s\n", token);
+    ranges[i][1].max = atoi(token);
     i++;
+    printf("%d\n",
+           range_overlaps_other_range(ranges[i][1].min, ranges[i][1].max,
+                                      ranges[i][0].min, ranges[i][0].max));
   }
-  for (int k = 0; k < 300; k += 3) {
-    answer += find_common_char_3_arrays(lines[k], lines[k + 1], lines[k + 2],
-                                        line_lenghts[k], line_lenghts[k + 1],
-                                        line_lenghts[k + 2]);
-    // answer +=
+  for (j = 0; j < 1000; j++) {
+    answer += range_overlaps_other_range(ranges[j][1].min, ranges[j][1].max,
+                                         ranges[j][0].min, ranges[j][0].max);
   }
-  printf("Answer: %d", answer);
+  printf("%d", answer);
   fclose(fptr);
   return 0;
 }
+
+// // day 3
+// int find_common_char(char *arr1, char *arr2, int len) {
+//   int i = 0, j = 0;
+//   for (i = 0; i < len; i++) {
+//     for (j = 0; j < len; j++) {
+//       if (arr1[i] == arr2[j]) {
+//         if (arr1[i] < 91) {
+//           printf("Common char: %c Value: %d\n", arr1[i],
+//                  (int)(arr1[i] - 38)); // use some ascii magic to get the
+//                  value
+//           return (int)(arr1[i] - 38);
+//         } else {
+//           printf("Common char: %c Value: %d\n", arr1[i], (int)(arr1[i] -
+//           96)); return (int)(arr1[i] - 96);
+//         }
+//       }
+//     }
+//   }
+//   return 1;
+// }
+
+// int find_common_char_3_arrays(char *arr1, char *arr2, char *arr3, int len1,
+//                               int len2, int len3) {
+//   int i = 0, j = 0, k = 0;
+//   for (i = 0; i < len1; i++) {
+//     for (j = 0; j < len2; j++) {
+//       for (k = 0; k < len3; k++) {
+//         if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+//           if (arr1[i] < 91) {
+//             // printf("Common char: %c Value: %d\n", arr1[i], (int)(arr1[i]
+//             -
+//             // 38));
+//             return (int)(arr1[i] - 38); // use some ascii magic to get the
+//             value
+//           } else {
+//             // printf("Common char: %c Value: %d\n", arr1[i], (int)(arr1[i]
+//             -
+//             // 96));
+//             return (int)(arr1[i] - 96);
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return 1;
+// }
+// // part 2
+// int main() {
+//   FILE *fptr;
+//   int answer = 0;
+//   char str[60];
+//   char lines[300][100];
+//   int line_lenghts[300];
+//   int i = 0;
+//   int j = 0;
+
+//   if ((fptr = fopen("C:\\input.txt", "r")) == NULL) {
+//     printf("Error! opening file");
+//     return 0;
+//   }
+
+//   while (fgets(str, 60, fptr) != NULL) { // put data in array
+//     while (str[j] != '\0') {
+//       lines[i][j] = str[j];
+//       j++;
+//     }
+//     if (str[j] == '\0') {
+//       lines[i][j - 1] = '\0';
+//       line_lenghts[i] = j - 1;
+//     }
+//     j = 0;
+//     i++;
+//   }
+//   for (int k = 0; k < 300; k += 3) {
+//     answer += find_common_char_3_arrays(lines[k], lines[k + 1], lines[k +
+//     2],
+//                                         line_lenghts[k], line_lenghts[k +
+//                                         1], line_lenghts[k + 2]);
+//     // answer +=
+//   }
+//   printf("Answer: %d", answer);
+//   fclose(fptr);
+//   return 0;
+// }
 
 // part 1
 //  int main() {
