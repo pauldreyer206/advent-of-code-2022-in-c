f2e7328e043a67338168f79b4ff7f9ed91eb9ed8
pauldreyer206 <paulroux.dreyer@gmail.com>
2022-12-05 22:24:49 +0200

    Day 5

--- main.c
+++ main.c
@@ -2,31 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// day 4
 
-int range_contains_other_range(int min, int max, int min2, int max2) { // part 1
-  return (min >= min2 && min <= max2) || (max >= min2 && max <= max2);
-}
+// day 5
 
-int range_overlaps_other_range(int min, int max, int min2, int max2) { // part 2
-  return (min >= min2 && min <= max2) || (max >= min2 && max <= max2) ||
-         (min2 >= min && min2 <= max) || (max2 >= min && max2 <= max);
-}
-
-struct range {
-  int min;
-  int max;
+//     [H]         [D]     [P]
+// [W] [B]         [C] [Z] [D]
+// [T] [J]     [T] [J] [D] [J]
+// [H] [Z]     [H] [H] [W] [S]     [M]
+// [P] [F] [R] [P] [Z] [F] [W]     [F]
+// [J] [V] [T] [N] [F] [G] [Z] [S] [S]
+// [C] [R] [P] [S] [V] [M] [V] [D] [Z]
+// [F] [G] [H] [Z] [N] [P] [M] [N] [D]
+//  1   2   3   4   5   6   7   8   9
+struct move_s {
+  int amount;
+  int from;
+  int to;
 };
+int string_length(char *str) {
+  int len = 0;
+  while (*str != '\0') {
+    len++;
+    str++;
+  }
+  return len;
+}
 
-struct range ranges[1000][2];
+void add_char(char *str, char c) { // not safe, don't care
+  int len = string_length(str);
+  str[len] = c;
+  str[len + 1] = '\0';
+}
 
-// part one
+char remove_char(char *str) { // not safe, don't care
+  int len = string_length(str);
+  char c = str[len - 1];
+  str[len - 1] = '\0';
+  return c;
+}
+
+void add_string(char *str, char *str2) { // not safe, don't care
+  int len = string_length(str);
+  int len2 = string_length(str2);
+  int i = 0;
+  for (i = 0; i < len2; i++) {
+    str[len + i] = str2[i];
+  }
+  str[len + len2] = '\0';
+}
+
 int main() {
   FILE *fptr;
+  struct move_s move;
+
+  char stacks[9][1000] = {"FCJPHTW", "GRVFZJBH", "HPTR", "ZSNPHT", "NVFZHJCD",
+                          "PMGFWDZ", "MVZWSJDP", "NDS",  "DZSFM"};
+
   char str[60];
   int i = 0;
-  int j = 0;
-  int answer = 0;
   char *token;
 
   if ((fptr = fopen("C:\\input.txt", "r")) == NULL) {
@@ -34,32 +67,94 @@ int main() {
     return 0;
   }
   while (fgets(str, 60, fptr) != NULL) { // put data in array
-    printf("%s", str);
-    token = strtok(str, ",-");
-    printf("%s\n", token);
-    ranges[i][0].min = atoi(token);
-    token = strtok(NULL, ",-");
-    printf("%s\n", token);
-    ranges[i][0].max = atoi(token);
-    token = strtok(NULL, ",-");
-    printf("%s\n", token);
-    ranges[i][1].min = atoi(token);
-    token = strtok(NULL, ",-");
-    printf("%s\n", token);
-    ranges[i][1].max = atoi(token);
-    i++;
-    printf("%d\n",
-           range_overlaps_other_range(ranges[i][1].min, ranges[i][1].max,
-                                      ranges[i][0].min, ranges[i][0].max));
+
+    // printf("%s", str);
+    token = strtok(str, "move");
+    move.amount = atoi(token);
+    // printf("start: %d \n", move.start);
+    token = strtok(NULL, "from");
+    move.from = atoi(token);
+    token = strtok(NULL, "to");
+    move.to = atoi(token);
+    printf("amount: %d from: %d to: %d \n", move.amount, move.from, move.to);
+    // for (i = 0; i < move.amount; i++) {//part 1
+    //   add_char(stacks[move.to - 1], remove_char(stacks[move.from - 1]));
+    // }
+    add_string(stacks[move.to - 1],
+               &stacks[move.from - 1][string_length(stacks[move.from - 1]) -
+                                      move.amount]); // part 2
+    stacks[move.from - 1][string_length(stacks[move.from - 1]) - move.amount] =
+        '\0'; // part 2
   }
-  for (j = 0; j < 1000; j++) {
-    answer += range_overlaps_other_range(ranges[j][1].min, ranges[j][1].max,
-                                         ranges[j][0].min, ranges[j][0].max);
+  for (i = 0; i < 9; i++) {
+    printf("%s \n", stacks[i]);
   }
-  printf("%d", answer);
   fclose(fptr);
   return 0;
 }
+
+// day 4
+
+// int range_contains_other_range(int min, int max, int min2, int max2) { //
+// part 1
+//   return (min >= min2 && min <= max2) || (max >= min2 && max <= max2);
+// }
+
+// int range_overlaps_other_range(int min, int max, int min2, int max2) { //
+// part 2
+//   return (min >= min2 && min <= max2) || (max >= min2 && max <= max2) ||
+//          (min2 >= min && min2 <= max) || (max2 >= min && max2 <= max);
+// }
+
+// struct range {
+//   int min;
+//   int max;
+// };
+
+// struct range ranges[1000][2];
+
+// // part one
+// int main() {
+//   FILE *fptr;
+//   char str[60];
+//   int i = 0;
+//   int j = 0;
+//   int answer = 0;
+//   char *token;
+
+//   if ((fptr = fopen("C:\\input.txt", "r")) == NULL) {
+//     printf("Error! opening file");
+//     return 0;
+//   }
+//   while (fgets(str, 60, fptr) != NULL) { // put data in array
+//     printf("%s", str);
+//     token = strtok(str, ",-");
+//     printf("%s\n", token);
+//     ranges[i][0].min = atoi(token);
+//     token = strtok(NULL, ",-");
+//     printf("%s\n", token);
+//     ranges[i][0].max = atoi(token);
+//     token = strtok(NULL, ",-");
+//     printf("%s\n", token);
+//     ranges[i][1].min = atoi(token);
+//     token = strtok(NULL, ",-");
+//     printf("%s\n", token);
+//     ranges[i][1].max = atoi(token);
+//     i++;
+//     printf("%d\n",
+//            range_overlaps_other_range(ranges[i][1].min, ranges[i][1].max,
+//                                       ranges[i][0].min, ranges[i][0].max));
+//   }
+//   for (j = 0; j < 1000; j++) {
+//     answer += range_overlaps_other_range(ranges[j][1].min,
+//     ranges[j][1].max,
+//                                          ranges[j][0].min,
+//                                          ranges[j][0].max);
+//   }
+//   printf("%d", answer);
+//   fclose(fptr);
+//   return 0;
+// }
 
 // // day 3
 // int find_common_char(char *arr1, char *arr2, int len) {
