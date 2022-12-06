b44f8eaac23857d296a201b6d18d0d0fc5b4a93d
pauldreyer206 <paulroux.dreyer@gmail.com>
2022-12-06 09:22:51 +0200

    Day 6

--- main.c
+++ main.c
@@ -2,6 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// day 6
+int find_n_unique_set(char *str, int len, int n) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  int found = 0;
+  for (i = 0; i < len - n; i++) {
+    found = 0;
+    for (j = 0; j < n; j++) {
+      for (k = 0; k < n; k++) {
+        if (str[i + j] == str[i + k]) {
+          found++;
+        }
+      }
+    }
+    if (found == n) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int main() {
+  FILE *fptr;
+  char chars[4096];
+  int answer;
+
+  if ((fptr = fopen("C:\\input.txt", "r")) == NULL) {
+    printf("Error! opening file");
+    return 0;
+  }
+  while (fgets(chars, 4096, fptr) != NULL) {
+    printf("%s", chars);
+  }
+  // answer = 4 + find_n_unique_set(chars, 4096, 4);//part 1
+  answer = 14 + find_n_unique_set(chars, 4096, 14); // part 2
+  printf("%d", answer);
+  fclose(fptr);
+  return 0;
+}
 
 // day 5
 
@@ -14,84 +54,86 @@
 // [C] [R] [P] [S] [V] [M] [V] [D] [Z]
 // [F] [G] [H] [Z] [N] [P] [M] [N] [D]
 //  1   2   3   4   5   6   7   8   9
-struct move_s {
-  int amount;
-  int from;
-  int to;
-};
-int string_length(char *str) {
-  int len = 0;
-  while (*str != '\0') {
-    len++;
-    str++;
-  }
-  return len;
-}
+// struct move_s {
+//   int amount;
+//   int from;
+//   int to;
+// };
+// int string_length(char *str) {
+//   int len = 0;
+//   while (*str != '\0') {
+//     len++;
+//     str++;
+//   }
+//   return len;
+// }
 
-void add_char(char *str, char c) { // not safe, don't care
-  int len = string_length(str);
-  str[len] = c;
-  str[len + 1] = '\0';
-}
+// void add_char(char *str, char c) { // not safe, don't care
+//   int len = string_length(str);
+//   str[len] = c;
+//   str[len + 1] = '\0';
+// }
 
-char remove_char(char *str) { // not safe, don't care
-  int len = string_length(str);
-  char c = str[len - 1];
-  str[len - 1] = '\0';
-  return c;
-}
+// char remove_char(char *str) { // not safe, don't care
+//   int len = string_length(str);
+//   char c = str[len - 1];
+//   str[len - 1] = '\0';
+//   return c;
+// }
 
-void add_string(char *str, char *str2) { // not safe, don't care
-  int len = string_length(str);
-  int len2 = string_length(str2);
-  int i = 0;
-  for (i = 0; i < len2; i++) {
-    str[len + i] = str2[i];
-  }
-  str[len + len2] = '\0';
-}
+// void add_string(char *str, char *str2) { // not safe, don't care
+//   int len = string_length(str);
+//   int len2 = string_length(str2);
+//   int i = 0;
+//   for (i = 0; i < len2; i++) {
+//     str[len + i] = str2[i];
+//   }
+//   str[len + len2] = '\0';
+// }
 
-int main() {
-  FILE *fptr;
-  struct move_s move;
+// int main() {
+//   FILE *fptr;
+//   struct move_s move;
 
-  char stacks[9][1000] = {"FCJPHTW", "GRVFZJBH", "HPTR", "ZSNPHT", "NVFZHJCD",
-                          "PMGFWDZ", "MVZWSJDP", "NDS",  "DZSFM"};
+//   char stacks[9][1000] = {"FCJPHTW", "GRVFZJBH", "HPTR", "ZSNPHT",
+//   "NVFZHJCD",
+//                           "PMGFWDZ", "MVZWSJDP", "NDS",  "DZSFM"};
 
-  char str[60];
-  int i = 0;
-  char *token;
+//   char str[60];
+//   int i = 0;
+//   char *token;
 
-  if ((fptr = fopen("C:\\input.txt", "r")) == NULL) {
-    printf("Error! opening file");
-    return 0;
-  }
-  while (fgets(str, 60, fptr) != NULL) { // put data in array
+//   if ((fptr = fopen("C:\\input.txt", "r")) == NULL) {
+//     printf("Error! opening file");
+//     return 0;
+//   }
+//   while (fgets(str, 60, fptr) != NULL) { // put data in array
 
-    // printf("%s", str);
-    token = strtok(str, "move");
-    move.amount = atoi(token);
-    // printf("start: %d \n", move.start);
-    token = strtok(NULL, "from");
-    move.from = atoi(token);
-    token = strtok(NULL, "to");
-    move.to = atoi(token);
-    printf("amount: %d from: %d to: %d \n", move.amount, move.from, move.to);
-    // for (i = 0; i < move.amount; i++) {//part 1
-    //   add_char(stacks[move.to - 1], remove_char(stacks[move.from - 1]));
-    // }
-    add_string(stacks[move.to - 1],
-               &stacks[move.from - 1][string_length(stacks[move.from - 1]) -
-                                      move.amount]); // part 2
-    stacks[move.from - 1][string_length(stacks[move.from - 1]) - move.amount] =
-        '\0'; // part 2
-  }
-  for (i = 0; i < 9; i++) {
-    printf("%s \n", stacks[i]);
-  }
-  fclose(fptr);
-  return 0;
-}
+//     // printf("%s", str);
+//     token = strtok(str, "move");
+//     move.amount = atoi(token);
+//     // printf("start: %d \n", move.start);
+//     token = strtok(NULL, "from");
+//     move.from = atoi(token);
+//     token = strtok(NULL, "to");
+//     move.to = atoi(token);
+//     printf("amount: %d from: %d to: %d \n", move.amount, move.from, move.to);
+//     // for (i = 0; i < move.amount; i++) {//part 1
+//     //   add_char(stacks[move.to - 1], remove_char(stacks[move.from - 1]));
+//     // }
+//     add_string(stacks[move.to - 1],
+//                &stacks[move.from - 1][string_length(stacks[move.from - 1]) -
+//                                       move.amount]); // part 2
+//     stacks[move.from - 1][string_length(stacks[move.from - 1]) - move.amount]
+//     =
+//         '\0'; // part 2
+//   }
+//   for (i = 0; i < 9; i++) {
+//     printf("%s \n", stacks[i]);
+//   }
+//   fclose(fptr);
+//   return 0;
+// }
 
 // day 4
 
